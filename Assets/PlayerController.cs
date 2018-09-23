36d4707385b41dd1574c9ca0b189ec0046ec01f9
DirtyDanChicago <32584314+DirtyDanChicago@users.noreply.github.com>
2018-09-23 01:07:25 -0500

    Okay, so much got messed up. Fixed win, and collection.
    So I genuinely don't know what happened here. I had to copy the entire code after something went wrong with mine. Spent a good 40 minutes scratching my head to no avail. The two sets of code that looked near identical. No idea what happened.

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,26 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
-public class PlayerController : MonoBehaviour {
-
+public class PlayerController : MonoBehaviour
+{
 
     public float speed;
-
     public Text countText;
+    public Text winText;
 
     private Rigidbody rb;
-
     private int count;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-
         count = 0;
-
         SetCountText();
+        winText.text = "";
     }
 
     void FixedUpdate()
@@ -35,12 +32,10 @@ public class PlayerController : MonoBehaviour {
 
     void OnTriggerEnter(Collider other)
     {
-       if (other.gameObject.CompareTag("PickUp"))
+        if (other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive(false);
-
             count = count + 1;
-
             SetCountText();
         }
     }
@@ -48,7 +43,9 @@ public class PlayerController : MonoBehaviour {
     void SetCountText()
     {
         countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You Win!";
+        }
     }
 }
-
-    
