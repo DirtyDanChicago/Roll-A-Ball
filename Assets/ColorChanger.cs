d83f1d70927931391928b758d44b3ef0e0e6223c
DirtyDanChicago <32584314+DirtyDanChicago@users.noreply.github.com>
2018-10-01 20:13:48 -0500

    Add color changer script for new feature
    I added a very, and I mean very, simple color changing script for a potential new feature. It's the same feature I want for my midterm.

---
+++ Assets/ColorChanger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour
+{
+
+    MeshRenderer meshRenderer;
+   
+
+    // Use this for initialization
+    void Start ()
+    {
+        //This should gather the color component of the player, and sets default to white.
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.material.color = Color.white;
+
+    }
+
+// Update is called once per frame
+    void Update ()
+    {
+        //If player hits Q, color turns yellow.
+        if ((Input.GetKey("q")))
+        {
+           meshRenderer.material.color = Color.yellow;
+        }
+
+        //If player hits E, color turns green.
+        if ((Input.GetKey("e")))
+        {
+            meshRenderer.material.color = Color.green;
+        }
+	}
+}
